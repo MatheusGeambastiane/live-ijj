f67b7c5a3b4ee012c308479169a0b2d0cefa0dce
Matheus Geambastiane <mgeambastiane@gmail.com>
2023-08-22 15:49:48 -0300

    Feature: aula e massa

--- aula1.py
+++ aula1.py
@@ -32,44 +32,59 @@ class Test_case():
 
         self.navegador.get("https://google.com")
 
-    def make_query(self):
+    def make_query(self, query):
         search_bar = self.get_element_name("q")
 
         search_bar.click()
-        search_bar.send_keys("Instituto joga junto")
+        search_bar.send_keys(query)
 
         ask_button = self.get_element_name( "btnK")
 
         ask_button.click()
 
-        self.verify_title("Instituto joga junto - Pesquisa Google")
+        self.verify_title(f"{query} - Pesquisa Google")
         ijj_result = self.get_element_xpath("/html/body/div[6]/div/div[12]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div/a/h3")
         ijj_result.click()
         self.verify_title("Instituto Joga Junto")
-        ...
+        return 
 
 
 
 
 
-
-        def form_section(self, name, mail, subject, text):
-            nome = self.get_element_name("nome")
-            nome.send_keys(name)
-            email = self.get_element_name("email")
-            email.send_keys("matheus.ramos@jogajuntoinstituto.org")
-            body = self.get_element_name("body")
-            body.send_keys("Teste Selenium - Aula 1")
-
-
-            assunto = self.get_element_name("assunto")
-
-            select = Select(assunto)
-
-            select.select_by_visible_text("Me inscrever em um curso")
-
-            body.submit()
+    def form_section(self, name, mail, text):
+        nome = self.get_element_name("nome")
+        nome.send_keys(name)
+        email = self.get_element_name("email")
+        email.send_keys(mail)
+        body = self.get_element_name("body")
+        body.send_keys(text)
 
 
-            def browser_quit():
-                self.navegador.quit()
+        assunto = self.get_element_name("assunto")
+
+        select = Select(assunto)
+
+        select.select_by_visible_text("Me inscrever em um curso")
+
+        body.submit()
+        return
+
+
+    def browser_quit(self):
+        self.navegador.quit()
+        return
+
+
+test = Test_case()
+test.open_browser()
+test.make_query("Instituto joga junto")
+nomes = ["Matheus", "Michelle", "Mariana"]
+
+# for nome in nomes:
+
+#     test.form_section(nome, "matheus@matheusfsefasefsaef.com", "Texto teste")
+
+sleep(3)
+test.browser_quit()
+
--- massa.py
+++ massa.py
@@ -1,6 +1,25 @@
 from faker import Faker
+import pandas as pd
+
 
 fake = Faker('pt_BR')
-print(fake.name())  # Gera um nome aleatório
-print(fake.text())  # Gera um endereço aleatório
-print(fake.email())
+
+def persona(quantidade):
+    personas = []
+    for _ in range(quantidade):
+        data = {
+            'nome': fake.name(),
+            'endereco': fake.address(),
+            'email': fake.email(),
+        }
+        personas.append(data)
+
+    print(f'Foram adicionadas {quantidade} personas.')
+    
+    # Transforma a lista de dicionários em um DataFrame
+    df = pd.DataFrame(personas)
+    
+    
+    
+    return df
+result = persona(20)
